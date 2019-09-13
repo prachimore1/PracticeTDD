7780681247655baaa7c10c658ba73e80e5223447
prachimore1 <prachi.more@integral.io>
2019-09-13 09:54:08 -0400

    Refactored PotterStoreTest

--- src/test/java/PotterStoreTest.java
+++ src/test/java/PotterStoreTest.java
@@ -1,36 +1,42 @@
 import com.java.Basket;
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
 
 public class PotterStoreTest {
+
+    Basket basketTest;
+
     @Test
     public void emptyBasketTest() {
-        Basket basket = new Basket();
-        double actual = basket.checkout();
+        basketTest = new Basket();
+        double actual = basketTest.checkout();
         assertTrue(0.0d - actual < 0.001);
     }
 
     @Test
     public void twoDiffBooksTest() {
-        Basket basket = new Basket();
-        basket.add(1);
-        basket.add(2);
-        double actual = basket.checkout();
+        basketTest = new Basket();
+        basketTest.add(1);
+        basketTest.add(2);
+        double actual = basketTest.checkout();
         assertTrue(8.0d - actual < 0.001);
     }
 
     @Test
     public void fiveDiffBooks() {
-        Basket basket = new Basket();
-        basket.add(1);
-        basket.add(3);
-        basket.add(4);
-        basket.add(2);
-        basket.add(5);
-        double actual = basket.checkout();
+        basketTest = new Basket();
+        basketTest.add(1);
+        basketTest.add(3);
+        basketTest.add(4);
+        basketTest.add(2);
+        basketTest.add(5);
+        double actual = basketTest.checkout();
         assertTrue(0.0d - actual < 0.001);
     }
 
-
+    @Test
+    public void twoSameBooksTest() {
+    }
 }
